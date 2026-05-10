b0dbcea92a975fcff8bad3e780e96153c3e9d7b0
mohamedsamak <mody.samak.2008@gmail.com>
2026-05-10 20:32:34 +0400

    Finished the UI setup

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +16,20 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100,pady=50, bg=YELLOW)
+
+canvas = Canvas(width=210, height=224, bg=YELLOW, highlightthickness=0)
+tomato_png = PhotoImage(file="tomato.png")
+canvas.create_image(103, 112, image=tomato_png)
+canvas.create_text(103,130,text="00:00",fill="white",font=(FONT_NAME, 27,"bold"))
+canvas.pack()
+
+
+
+
+
+
+
+window.mainloop()
