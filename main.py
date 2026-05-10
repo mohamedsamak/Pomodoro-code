49fe865485c9f0141dadc2136f39d04fae18d86d
mohamedsamak <mody.samak.2008@gmail.com>
2026-05-10 21:42:57 +0400

    Further updates on the UI

--- main.py
+++ main.py
@@ -20,11 +20,14 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100,pady=50, bg=YELLOW)
 
-canvas = Canvas(width=210, height=224, bg=YELLOW, highlightthickness=0)
+title_label= Label(text="Timer",fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+title_label.grid(column=0, row=0)
+
+canvas = Canvas(width=220, height=224, bg=YELLOW, highlightthickness=0)
 tomato_png = PhotoImage(file="tomato.png")
-canvas.create_image(103, 112, image=tomato_png)
-canvas.create_text(103,130,text="00:00",fill="white",font=(FONT_NAME, 27,"bold"))
-canvas.pack()
+canvas.create_image(110, 112, image=tomato_png)
+canvas.create_text(110,130,text="00:00",fill="white",font=(FONT_NAME, 27,"bold"))
+canvas.grid()
 
 
 
